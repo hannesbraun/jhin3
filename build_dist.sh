0a188b7464af2fdce8be35ea57e9037b7c88b5f1
Hannes Braun <hannesbraun@mail.de>
2020-03-14 13:04:04 +0100

    Update build script

--- build_dist.sh
+++ build_dist.sh
@@ -5,8 +5,9 @@ VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpress
 mvn package
 mkdir jhin3-$VERSION
 cp target/jhin3-$VERSION-jar-with-dependencies.jar jhin3-$VERSION/jhin3-$VERSION.jar
-cp LICENSE jhin3-$VERSION/
-cp NOTICE jhin3-$VERSION/
+cp LICENSE jhin3-$VERSION/LICENSE.txt
+cp NOTICE jhin3-$VERSION/NOTICE.txt
+cp CHANGELOG jhin3-$VERSION/CHANGELOG.txt
 cd manual && make && cd ..
 cp manual/Jhin3\ user\ manual.pdf jhin3-$VERSION/
 cp sample_conf.json jhin3-$VERSION/
